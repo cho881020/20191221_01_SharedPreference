0f07f6c9c19cf414c428f6342fdf20ad627c44c4
cho881020 <kj_cho@idealidea.co.kr>
2019-12-21 10:48:44 +0900

    로그인버튼이 눌리고 / ID저장 체크가 되어있다면 => 실제로 ID를 SharedPreference에 저장

--- app/src/main/java/com/tjeit/a20191221_01_sharedpreference/MainActivity.kt
+++ app/src/main/java/com/tjeit/a20191221_01_sharedpreference/MainActivity.kt
@@ -2,6 +2,9 @@ package com.tjeit.a20191221_01_sharedpreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.tjeit.a20191221_01_sharedpreference.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +16,22 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        Q1. 아이디 저장 로직 구현
+//        loginBtn이 눌리면
+//        idCheckBox가 체크되어있는지 검사 (구글링)
+//        되어 있다면 idEdt에 적혀있는 값을 userId 라는 val로 저장
+
+        loginBtn.setOnClickListener {
+            if (idCheckBox.isChecked) {
+                val userId = idEdt.text.toString()
+
+                ContextUtil.setUserId(mContext, userId)
+
+
+            }
+        }
+
 
     }
 
